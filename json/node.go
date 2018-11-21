ed290df356a23145759b7c260749a60e4ae8ab65
Felix Hanley <felix@userspace.com.au>
2018-11-21 15:57:28 +1100

    Split JSON parser

--- json/node.go
+++ json/node.go
@@ -2,11 +2,8 @@ package json
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"io"
-	"sort"
-	"strconv"
 
 	base "src.userspace.com.au/query"
 )
@@ -97,73 +94,4 @@ func (n *Node) SelectElement(name string) *Node {
 		}
 	}
 	return nil
-}
-
-func parseValue(x interface{}, top *Node, level int) {
-	addNode := func(n *Node) {
-		if n.level == top.level {
-			top.nextSibling = n
-			n.prevSibling = top
-			n.parent = top.parent
-			if top.parent != nil {
-				top.parent.lastChild = n
-			}
-		} else if n.level > top.level {
-			n.parent = top
-			if top.firstChild == nil {
-				top.firstChild = n
-				top.lastChild = n
-			} else {
-				t := top.lastChild
-				t.nextSibling = n
-				n.prevSibling = t
-				top.lastChild = n
-			}
-		}
-	}
-
-	// TODO check for null
-
-	switch v := x.(type) {
-	case []interface{}:
-		for _, vv := range v {
-			n := &Node{nodeType: base.ElementNode, level: level, dataType: "arrayitem"}
-			addNode(n)
-			parseValue(vv, n, level+1)
-		}
-	case map[string]interface{}:
-		var keys []string
-		for key := range v {
-			keys = append(keys, key)
-		}
-		sort.Strings(keys)
-		for _, key := range keys {
-			n := &Node{data: key, nodeType: base.ElementNode, level: level, dataType: "object"}
-			addNode(n)
-			parseValue(v[key], n, level+1)
-		}
-	case string:
-		n := &Node{data: v, nodeType: base.TextNode, level: level, dataType: "string"}
-		addNode(n)
-	case float64:
-		s := strconv.FormatFloat(v, 'f', -1, 64)
-		n := &Node{data: s, nodeType: base.TextNode, level: level, dataType: "number"}
-		addNode(n)
-	case bool:
-		s := strconv.FormatBool(v)
-		n := &Node{data: s, nodeType: base.TextNode, level: level, dataType: "boolean"}
-		addNode(n)
-	}
-}
-
-// Parse JSON document.
-func Parse(r io.Reader) (*Node, error) {
-	var v interface{}
-	decoder := json.NewDecoder(r)
-	if err := decoder.Decode(&v); err != nil {
-		return nil, err
-	}
-	doc := &Node{nodeType: base.DocumentNode}
-	parseValue(v, doc, 1)
-	return doc, nil
 }
---
+++ json/parse.go
@@ -0,0 +1,79 @@
+package json
+
+import (
+	"encoding/json"
+	"io"
+	"sort"
+	"strconv"
+
+	base "src.userspace.com.au/query"
+)
+
+// Parse JSON document.
+func Parse(r io.Reader) (*Node, error) {
+	var v interface{}
+	decoder := json.NewDecoder(r)
+	if err := decoder.Decode(&v); err != nil {
+		return nil, err
+	}
+	doc := &Node{nodeType: base.DocumentNode}
+	parseValue(v, doc, 1)
+	return doc, nil
+}
+
+func parseValue(x interface{}, top *Node, level int) {
+	addNode := func(n *Node) {
+		if n.level == top.level {
+			top.nextSibling = n
+			n.prevSibling = top
+			n.parent = top.parent
+			if top.parent != nil {
+				top.parent.lastChild = n
+			}
+		} else if n.level > top.level {
+			n.parent = top
+			if top.firstChild == nil {
+				top.firstChild = n
+				top.lastChild = n
+			} else {
+				t := top.lastChild
+				t.nextSibling = n
+				n.prevSibling = t
+				top.lastChild = n
+			}
+		}
+	}
+
+	// TODO check for null
+
+	switch v := x.(type) {
+	case []interface{}:
+		for _, vv := range v {
+			n := &Node{nodeType: base.ElementNode, level: level, dataType: "arrayitem"}
+			addNode(n)
+			parseValue(vv, n, level+1)
+		}
+	case map[string]interface{}:
+		var keys []string
+		for key := range v {
+			keys = append(keys, key)
+		}
+		sort.Strings(keys)
+		for _, key := range keys {
+			n := &Node{data: key, nodeType: base.ElementNode, level: level, dataType: "object"}
+			addNode(n)
+			parseValue(v[key], n, level+1)
+		}
+	case string:
+		n := &Node{data: v, nodeType: base.TextNode, level: level, dataType: "string"}
+		addNode(n)
+	case float64:
+		s := strconv.FormatFloat(v, 'f', -1, 64)
+		n := &Node{data: s, nodeType: base.TextNode, level: level, dataType: "number"}
+		addNode(n)
+	case bool:
+		s := strconv.FormatBool(v)
+		n := &Node{data: s, nodeType: base.TextNode, level: level, dataType: "boolean"}
+		addNode(n)
+	}
+}
